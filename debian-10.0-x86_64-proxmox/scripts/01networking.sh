07c13f3e9a46fe986a547defb9ef4c11d07f4355
Christian Wagner <chriswayg@gmail.com>
2019-08-30 02:01:27 +0800

    better way to show IP

--- debian-10.0-x86_64-proxmox/scripts/01networking.sh
+++ debian-10.0-x86_64-proxmox/scripts/01networking.sh
@@ -5,7 +5,6 @@ echo "Create a script which will run when the network comes up to display IP bef
 cp /etc/issue /etc/issue.original
 
 cat > /etc/network/if-up.d/show-ip-address << "EOF"
-#!/bin/sh
 #!/bin/bash
 if [ "$METHOD" = loopback ]; then
     exit 0
@@ -20,10 +19,8 @@ fi
 if [ "$IFACE" = ens18 ]; then
 cp /etc/issue.original /etc/issue
 #printf "Method: $METHOD, Mode: $MODE,  Interface: $IFACE\n\n" >> /etc/issue
-echo "SSH key fingerprint: \n$(ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub)" >> /etc/issue
-echo ""  >> /etc/issue
-echo "Server IP: $(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')" >> /etc/issue
-echo ""  >> /etc/issue
+printf "SSH key fingerprint: \n$(ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub)\n\n" >> /etc/issue
+printf "Server Network Interface: $(ip -4 -br addr |  sed -n '2p')\n\n" >> /etc/issue
 fi
 EOF
 
