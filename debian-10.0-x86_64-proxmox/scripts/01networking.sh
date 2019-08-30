7a6ec6fa6af8e1884ffbafc0572934c85872fe9b
Christian Wagner <chriswayg@gmail.com>
2019-08-30 14:11:33 +0800

    nicer code to get IP

--- debian-10.0-x86_64-proxmox/scripts/01networking.sh
+++ debian-10.0-x86_64-proxmox/scripts/01networking.sh
@@ -17,11 +17,9 @@ fi
 
 # Only run from main interface.
 if [ "$IFACE" = ens18 ]; then
-cp /etc/issue.original /etc/issue
-#printf "Method: $METHOD, Mode: $MODE,  Interface: $IFACE\n\n" >> /etc/issue
-printf "SSH key fingerprint: \n$(ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub)\n\n" >> /etc/issue
-printf "Server Network Interface: $(ip -4 -br addr |  sed -n '2p')\n\n" >> /etc/issue
-fi
+  cp /etc/issue.original /etc/issue
+  printf "SSH key fingerprint: \n$(ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub)\n\n" >> /etc/issue
+  printf "Server Network Interface: $(ip -4 -br addr |  sed -n '2p')\n\n" >> /etc/issue
 EOF
 
 chmod +x /etc/network/if-up.d/show-ip-address
