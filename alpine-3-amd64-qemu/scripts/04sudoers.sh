a16044e0cbd782bc9f68595eb1e3db72efeed1e3
Christian Wagner <chriswayg@gmail.com>
2020-04-15 20:18:02 +0800

    allow root login via console

--- alpine-3-amd64-qemu/scripts/04sudoers.sh
+++ alpine-3-amd64-qemu/scripts/04sudoers.sh
@@ -14,10 +14,10 @@ echo "%wheel ALL=NOPASSWD:ALL" >> /etc/sudoers
 
 # allows ssh login without the user being locked
 # https://unix.stackexchange.com/questions/193066/how-to-unlock-account-for-public-key-ssh-authorization-but-not-for-password-aut
-#usermod -p '*' alpine
+usermod -p '*' alpine
 
 # the root user is also being blocked from ssh login in sshd_config
-# remove root password
+# remove root password (optional - makes it impossible to login as root from console)
 #usermod -p '*' root
 
 # lock root account
