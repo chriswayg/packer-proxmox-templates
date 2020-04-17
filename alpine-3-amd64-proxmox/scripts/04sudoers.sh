f8d82f774c693336b7c5bbb87188b84e59622d79
Christian Wagner <chriswayg@gmail.com>
2020-04-17 13:19:00 +0800

    keep default root password for console login

--- alpine-3-amd64-proxmox/scripts/04sudoers.sh
+++ alpine-3-amd64-proxmox/scripts/04sudoers.sh
@@ -8,20 +8,17 @@ apk add sudo shadow
 # Create Initial User
 adduser -D alpine -G wheel
 
-# # allow sudo without password
-# echo "Defaults exempt_group=wheel" > /etc/sudoers
-# echo "%wheel ALL=NOPASSWD:ALL" >> /etc/sudoers
+# allow sudo without password
+echo "Defaults exempt_group=wheel" > /etc/sudoers
+echo "%wheel ALL=NOPASSWD:ALL" >> /etc/sudoers
 
-echo 'alpine  ALL=(ALL:ALL) NOPASSWD: ALL' >> /target/etc/sudoers.d/default-user
-chmod 440 /target/etc/sudoers.d/default-user
-
-# # allows ssh login without the user being locked
-# # https://unix.stackexchange.com/questions/193066/how-to-unlock-account-for-public-key-ssh-authorization-but-not-for-password-aut
-# usermod -p '*' alpine
+# allows ssh login without the user being locked
+# https://unix.stackexchange.com/questions/193066/how-to-unlock-account-for-public-key-ssh-authorization-but-not-for-password-aut
+usermod -p '*' alpine
 
 # the root user is also being blocked from ssh login in sshd_config
-# remove root password
-usermod -p '*' root
+# remove root password (optional - makes it impossible to login as root from console)
+#usermod -p '*' root
 
 # lock root account
 #passwd -l root
