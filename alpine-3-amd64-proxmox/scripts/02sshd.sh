c8355ae5b92d414821e1db945cbacc8d6450f19e
Christian Wagner <chriswayg@gmail.com>
2020-04-17 13:18:22 +0800

    secure ssh

--- alpine-3-amd64-proxmox/scripts/02sshd.sh
+++ alpine-3-amd64-proxmox/scripts/02sshd.sh
@@ -2,10 +2,7 @@ set -eux
 
 # RootLogin without password was permitted in order to allow packer ssh access
 # to provision the system. Its removed here to make the server more secure.
-sed -i "/^PermitRootLogin/c\PermitRootLogin no" /etc/ssh/sshd_config
-
-# UseDNS value is No which avoids login delays when the remote client's DNS cannot be resolved
-sed -i "/^UseDNS/c\UseDNS no" /etc/ssh/sshd_config
+sed -i '/^PermitRootLogin no/d' /etc/ssh/sshd_config
 
 # disable ssh password authentication
-sed -i "/^PasswordAuthentication/c\PasswordAuthentication no" /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
