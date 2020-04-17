c39c3b1ffba3f2cc802e75d9469f8c8cb76d38b4
Christian Wagner <chriswayg@gmail.com>
2020-04-17 13:22:10 +0800

    use better show ip method

--- alpine-3-amd64-proxmox/scripts/90showip.sh
+++ alpine-3-amd64-proxmox/scripts/90showip.sh
@@ -16,7 +16,8 @@ fi
 
 cp /etc/issue-standard /etc/issue
 printf "ECDSA key fingerprint:\n$(ssh-keygen -l -f /etc/ssh/ssh_host_ecdsa_key.pub)\n" >> /etc/issue
-printf "\nSSH user: alpine     Server IP: $(ip -o route get to 1.1.1.1 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')\n\n" >> /etc/issue
+printf "\nSSH user: alpine     IP: $(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')\n\n" >> /etc/issue
+
 EOF
 
 chmod +x /etc/network/if-up.d/show-ip-address
