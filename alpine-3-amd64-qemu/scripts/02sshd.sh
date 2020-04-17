ef1a9071e86485b3d046aa096e563adff0391fb1
Christian Wagner <chriswayg@gmail.com>
2020-04-17 13:20:35 +0800

    secure ssh

--- alpine-3-amd64-qemu/scripts/02sshd.sh
+++ alpine-3-amd64-qemu/scripts/02sshd.sh
@@ -2,7 +2,7 @@ set -eux
 
 # RootLogin without password was permitted in order to allow packer ssh access
 # to provision the system. Its removed here to make the server more secure.
-sed -i '/^PermitRootLogin yes/d' /etc/ssh/sshd_config
+sed -i '/^PermitRootLogin no/d' /etc/ssh/sshd_config
 
 # disable ssh password authentication
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
