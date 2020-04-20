e2640df27145bc9e33849edbe47dc4e920fbc36a
Christian Wagner <chriswayg@gmail.com>
2020-04-20 21:58:48 +0800

    removed

--- alpine-3-amd64-proxmox/scripts/70random.sh
+++
@@ -1,6 +0,0 @@
-set -eux
-
-# Fixes: Boot delay/issues because of limited entropy (fixed in Alpine 3.11)
-# https://gitlab.alpinelinux.org/alpine/aports/issues/9960
-#apk add haveged
-#rc-update add haveged boot
