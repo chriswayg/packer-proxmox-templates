42ea651c6de48c350b1c5c6692ffe0d48f9ef7f3
Christian Wagner <chriswayg@gmail.com>
2020-04-20 21:59:20 +0800

    activate serial console

--- alpine-3-amd64-proxmox/scripts/80cloudutils.sh
+++ alpine-3-amd64-proxmox/scripts/80cloudutils.sh
@@ -1,36 +1,6 @@
 set -eux
 
-# install cloud-init
-# apk add py3-zipp@edge
-# apk add py3-importlib-metadata@edgecommunity
-# apk add cloud-init@testing
-
-# Start Cloud-Init on Boot
-#rc-update add cloud-init default
-
-# make sure CD drive with cloud-init config data gets mounted
-# /dev/sr0        /media/cdrom    iso9660 ro 0 0
-#sed -i 's/\/dev\/cdrom/\/dev\/sr0/g' /etc/fstab
-#sed -i 's/noauto,ro/ro/g' /etc/fstab
-
-# writing of network config is not implemented in alpine cloud-init
-#apk add iproute2 ifupdown
-# echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
-
-# enable automatically growing the partition
-# cat > /etc/cloud/cloud.cfg.d/10_growpart.cfg << "EOF"
-# growpart:
-#   mode: growpart
-#   devices: ["/dev/sda2"]
-#   ignore_growroot_disabled: false
-#
-# # the above settings do not seem to get used, thus run growpart here
-# bootcmd:
-#  - growpart /dev/sda 2
-# EOF
-
 # install prerequisites for cloud-utils
-#apk add eudev        # OpenRC compatible fork of systemd-udev
 apk add nettle@edge  # A low-level cryptographic library
 apk add gnutls@edge  # A TLS protocol implementation
 
@@ -42,7 +12,7 @@ apk add acpi # ACPI client for battery, power, and thermal readings
 apk add nano
 
 # creates a growpart script which will run on startup
-cat > /etc/local.d/auto_grow_partition.start << "EOF"
+cat > /etc/local.d/05_auto_grow_partition.start << "EOF"
 #!/bin/sh
 growpart -N /dev/sda 2
 if [ $? -eq 0 ]; then
@@ -51,12 +21,14 @@ if [ $? -eq 0 ]; then
     resize2fs /dev/sda2
 fi
 EOF
-chmod +x /etc/local.d/auto_grow_partition.start
+chmod +x /etc/local.d/05_auto_grow_partition.start
+
+rc-update add local default
+
+# this prevents lots of tty messages to be logged to syslog
+sed -i 's/^tty/# tty/g' /etc/inittab
 
 # activate serial console
-# it is buggy, as it makes it hard to log in
-
-# sed -i 's/quiet/console=ttyS0,9600/g' /etc/update-extlinux.conf
-# update-extlinux
-#
-# sed -i 's/#ttyS0/ttyS0/g' /etc/inittab
+sed -i 's/quiet/console=ttyS0,9600/g' /etc/update-extlinux.conf
+update-extlinux
+sed -i 's/#ttyS0/ttyS0/g' /etc/inittab
