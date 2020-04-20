fdbda2e05b9fcc4ef1f00f576c5fef9158645857
Christian Wagner <chriswayg@gmail.com>
2020-04-20 21:17:06 +0800

    added auto_grow_partition script, changed name

--- alpine-3-amd64-proxmox/scripts/80cloudutils.sh
+++ alpine-3-amd64-proxmox/scripts/80cloudutils.sh
@@ -30,7 +30,7 @@ set -eux
 # EOF
 
 # install prerequisites for cloud-utils
-apk add eudev        # OpenRC compatible fork of systemd-udev
+#apk add eudev        # OpenRC compatible fork of systemd-udev
 apk add nettle@edge  # A low-level cryptographic library
 apk add gnutls@edge  # A TLS protocol implementation
 
@@ -40,6 +40,18 @@ apk add cloud-utils@testing
 # install utils
 apk add acpi # ACPI client for battery, power, and thermal readings
 apk add nano
+
+# creates a growpart script which will run on startup
+cat > /etc/local.d/auto_grow_partition.start << "EOF"
+#!/bin/sh
+growpart -N /dev/sda 2
+if [ $? -eq 0 ]; then
+    echo "* auto-growing and resizing /dev/sda2"
+    growpart /dev/sda 2
+    resize2fs /dev/sda2
+fi
+EOF
+chmod +x /etc/local.d/auto_grow_partition.start
 
 # activate serial console
 # it is buggy, as it makes it hard to log in
