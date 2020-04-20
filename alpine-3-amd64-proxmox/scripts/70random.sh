b4b160ed67ed2b170a37f1955d540ec6c7e80c0c
Christian Wagner <chriswayg@gmail.com>
2020-04-20 21:17:27 +0800

    try without haveged

--- alpine-3-amd64-proxmox/scripts/70random.sh
+++ alpine-3-amd64-proxmox/scripts/70random.sh
@@ -1,6 +1,6 @@
 set -eux
 
-# Fixes: Boot delay/issues because of limited entropy
+# Fixes: Boot delay/issues because of limited entropy (fixed in Alpine 3.11)
 # https://gitlab.alpinelinux.org/alpine/aports/issues/9960
-apk add haveged
-rc-update add haveged boot
+#apk add haveged
+#rc-update add haveged boot
