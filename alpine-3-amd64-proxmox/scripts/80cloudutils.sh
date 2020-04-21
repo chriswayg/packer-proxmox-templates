ab5f6b70d9424010b300e93451bf30b6d80abd6d
Christian Wagner <chriswayg@gmail.com>
2020-04-21 20:11:48 +0800

    add logging for auto_grow_part

--- alpine-3-amd64-proxmox/scripts/80cloudutils.sh
+++ alpine-3-amd64-proxmox/scripts/80cloudutils.sh
@@ -1,34 +1,31 @@
 set -eux
 
-# install prerequisites for cloud-utils
-apk add nettle@edge  # A low-level cryptographic library
-apk add gnutls@edge  # A TLS protocol implementation
-
-# includes 'growpart'
-apk add cloud-utils@testing
-
-# install utils
-apk add acpi # ACPI client for battery, power, and thermal readings
+# install packages
+apk add nettle@edge     # A low-level cryptographic library (prereq for cloud-utils)
+apk add gnutls@edge     # A TLS protocol implementation (prereq for cloud-utils)
+apk add cloud-utils@testing # Useful set of utilities for interacting with a cloud (growpart)
+apk add e2fsprogs-extra # Ext2/3/4 filesystem extra utilities (resize2fs)
 apk add nano
 
 # creates a growpart script which will run on startup
 cat > /etc/local.d/05_auto_grow_partition.start << "EOF"
 #!/bin/sh
-growpart -N /dev/sda 2
+date  >>/var/log/growpart 2>&1
+growpart -N /dev/sda 2 >>/var/log/growpart 2>&1
 if [ $? -eq 0 ]; then
-    echo "* auto-growing and resizing /dev/sda2"
-    growpart /dev/sda 2
-    resize2fs /dev/sda2
+    echo "* auto-growing and resizing /dev/sda2" >>/var/log/growpart 2>&1
+    growpart /dev/sda 2 >>/var/log/growpart 2>&1
+    resize2fs /dev/sda2 >>/var/log/growpart 2>&1
 fi
 EOF
-chmod +x /etc/local.d/05_auto_grow_partition.start
+chmod -v +x /etc/local.d/05_auto_grow_partition.start
 
 rc-update add local default
-
-# this prevents lots of tty messages to be logged to syslog
-sed -i 's/^tty/# tty/g' /etc/inittab
 
 # activate serial console
 sed -i 's/quiet/console=ttyS0,9600/g' /etc/update-extlinux.conf
 update-extlinux
+
+# prevent lots of tty messages to be logged to syslog, only use serial console
+sed -i 's/^tty/# tty/g' /etc/inittab
 sed -i 's/#ttyS0/ttyS0/g' /etc/inittab
