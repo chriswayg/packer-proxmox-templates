e3dc6179f7b60465c8992c5014405958a3857412
Christian Wagner <chriswayg@gmail.com>
2020-04-27 01:08:40 +0800

    remove shell scripts, replaced by Ansible role

--- alpine-3-amd64-proxmox/scripts/00base.sh
+++
@@ -1,9 +0,0 @@
-set -ux
-
-# MOVED TO ANSIBLE
-
-apk upgrade -U --available
-
-source /etc/os-release
-
-printf "\n $PRETTY_NAME ($VERSION_ID) Cloud Server\n\n" > /etc/motd
--- alpine-3-amd64-proxmox/scripts/01networking.sh
+++
@@ -1,9 +0,0 @@
-set -ux
-
-# nothing special required
-
-# MOVED TO ANSIBLE
-
-echo "nothing to do"
-
-exit 0
